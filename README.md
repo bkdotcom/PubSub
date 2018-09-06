333fc18a4e8a45760b6169cb18f5877369941330
Brad Kent <bkfake-github@yahoo.com>
2018-09-05 22:51:20 -0500

    initial commit... sub-module'd from bdk/debug

---
+++ README.md
@@ -0,0 +1,21 @@
+Event Manager (Publish / Subscribe)
+===============
+
+### Installation
+This library requires PHP 5.4 (function array dereferencing, closure $this support) or later and has no userland dependencies.
+
+It is installable and autoloadable via [Composer](https://getcomposer.org/) as [bdk/pubsub](https://packagist.org/packages/bdk/pubsub).
+
+```json
+{
+    "require": {
+        "bdk/pubsub": "^2.0",
+    }
+}
+```
+
+See http://www.bradkent.com/php/pubsub for more information
+
+### Usage
+
+See http://www.bradkent.com/php/pubsub
---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+// backward compatibility
+$classMap = array(
+    // PHP 5.3 doesn't like leading backslash
+    'PHPUnit_Framework_Exception' => 'PHPUnit\Framework\Exception',
+    'PHPUnit_Framework_TestCase' => 'PHPUnit\Framework\TestCase',
+    'PHPUnit_Framework_TestSuite' => 'PHPUnit\Framework\TestSuite',
+);
+foreach ($classMap as $old => $new) {
+    if (!class_exists($new)) {
+        class_alias($old, $new);
+    }
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+\bdk\Debug::getInstance(array(
+	'objectsExclude' => array('PHPUnit_Framework_TestSuite', 'PHPUnit\Framework\TestSuite'),
+));
+*/
